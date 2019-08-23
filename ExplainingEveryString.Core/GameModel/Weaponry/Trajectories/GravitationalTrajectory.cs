5d87e23f6bba567879a15cb4a5463b602377dca6
chelovekbeznika <chelovekbeznika@yandex.ru>
2019-08-23 20:32:50 +0300

    Added gravitational trajectory.

---
+++ ExplainingEveryString.Core/GameModel/Weaponry/Trajectories/GravitationalTrajectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ExplainingEveryString.Core.GameModel.Weaponry.Trajectories
+{
+    internal class GravitationalTrajectory : BulletTrajectory
+    {
+        private Vector2 startSpeed;
+        private Single fallAcceleration;
+
+        internal GravitationalTrajectory(Vector2 startPosition, Vector2 fireDirection, Dictionary<String, Single> parameters)
+            : base(startPosition, fireDirection, parameters) { }
+
+        protected override void AssignParameters(Dictionary<String, Single> parameters)
+        {
+            startSpeed = new Vector2 { X = parameters["startFlySpeed"], Y = parameters["sideSpeed"] };
+            fallAcceleration = parameters["fallAcceleration"];
+        }
+
+        protected override Vector2 GetTrajectoryOffset(Single time)
+        {
+            return new Vector2
+            {
+                X = (startSpeed.X - fallAcceleration * time) * time,
+                Y = startSpeed.Y * time
+            };
+        }
+    }
+}
