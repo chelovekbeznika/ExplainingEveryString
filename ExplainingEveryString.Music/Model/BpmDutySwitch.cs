7ec8b023013b0fb459b0512de9df7fbc9a2f4758
chelovekbeznika <chelovekbeznika@yandex.ru>
2020-02-17 22:17:07 +0300

    Added BpmDutySwitch and extended boss-fight music.

---
+++ ExplainingEveryString.Music/Model/BpmDutySwitch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExplainingEveryString.Music.Model
+{
+    public class BpmDutySwitch : BpmSoundDirectingEvent
+    {
+        public Boolean FirstChannel { get; set; }
+        public Int32 Duty { get; set; }
+
+        public override IEnumerable<RawSoundDirectingEvent> GetEvents()
+        {
+            yield return new RawSoundDirectingEvent
+            {
+                Seconds = Seconds,
+                SamplesOffset = SamplesOffset,
+                SoundComponent = FirstChannel ? SoundComponentType.Pulse1 : SoundComponentType.Pulse2,
+                Parameter = SoundChannelParameter.Duty,
+                Value = Duty
+            };
+            yield break;
+        }
+    }
+}
