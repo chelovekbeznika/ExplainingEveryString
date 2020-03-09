73bc89552703ddf097ee88be4c8e2c7dae5b3194
chelovekbeznika <chelovekbeznika@yandex.ru>
2020-03-09 22:58:12 +0300

    Some basic assets for levels 2-X.

---
+++ Assets/Ready/TileSets/2x.tsx
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="2x_tiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="2x_tiles.png" width="256" height="256"/>
+ <tile id="0" probability="5"/>
+ <tile id="48">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
