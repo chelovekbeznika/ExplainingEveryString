63f7d5fa52b8e8838fa2816869950eddfd307d8f
chelovekbeznika <chelovekbeznika@yandex.ru>
2021-07-12 22:32:41 +0300

    Added level 4-1 tiled map.

---
+++ Assets/Ready/TileSets/4x.tsx
@@ -0,0 +1,364 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.0" name="4x" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="4x_tiles.png" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="Water floor" tile="0"/>
+  <terrain name="Thin walls" tile="70"/>
+  <terrain name="Thick walls" tile="70"/>
+  <terrain name="Algae" tile="76"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,1">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1" terrain=",,1,1">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2" terrain=",,1,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3" terrain=",1,1,1">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4" terrain="1,,1,1">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5" terrain=",,,3">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6" terrain=",,3,3">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7" terrain=",,3,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16" terrain=",1,,1">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17" terrain="1,1,1,1"/>
+ <tile id="18" terrain="1,,1,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19" terrain="1,1,,1">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20" terrain="1,1,1,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="21" terrain=",3,,3">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22" terrain="3,3,3,3">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="23" terrain="3,,3,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="32" terrain=",1,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33" terrain="1,1,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34" terrain="1,,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35" terrain=",0,0,0"/>
+ <tile id="36" terrain="0,,0,0"/>
+ <tile id="37" terrain=",3,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38" terrain="3,3,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="39" terrain="3,,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48" terrain=",,,0"/>
+ <tile id="49" terrain=",,0,0"/>
+ <tile id="50" terrain=",,0,"/>
+ <tile id="51" terrain="0,0,,0"/>
+ <tile id="52" terrain="0,0,0,"/>
+ <tile id="53" probability="83">
+  <properties>
+   <property name="Pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64" terrain=",0,,0"/>
+ <tile id="65" terrain="0,0,0,0"/>
+ <tile id="66" terrain="0,,0,"/>
+ <tile id="67" terrain="2,2,2,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="68" terrain="2,2,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="69" terrain="2,2,,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="70" terrain=",,,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="71" terrain=",,2,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="72" terrain=",,2,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="80" terrain=",0,,"/>
+ <tile id="81" terrain="0,0,,"/>
+ <tile id="82" terrain="0,,,"/>
+ <tile id="83" terrain="2,,2,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="85" terrain=",2,,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="86" terrain=",2,,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="87" terrain="2,2,2,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="88" terrain="2,,2,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="99" terrain="2,,2,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100" terrain=",,2,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="101" terrain=",2,2,2">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="102" terrain=",2,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="103" terrain="2,2,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="104" terrain="2,,,">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="Wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
