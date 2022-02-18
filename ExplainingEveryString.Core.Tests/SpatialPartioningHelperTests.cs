7dcf77e93486aff9614582015b43ac8cd811d8ed
chelovekbeznika <chelovekbeznika@yandex.ru>
2022-02-18 22:38:55 +0200

    Added possibility to check in which partitioning sectors are line.

---
+++ ExplainingEveryString.Core.Tests/SpatialPartioningHelperTests.cs
@@ -0,0 +1,59 @@
+﻿using ExplainingEveryString.Core.Collisions;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplainingEveryString.Core.Tests
+{
+    [TestFixture]
+    public class SpatialPartioningHelperTests
+    {
+        [Test]
+        [TestCase(15, 20, 40, 40)]
+        [TestCase(40, 15, 10, 40)]
+        public void GetSectors_TwoPointsInOneSector_ShouldReturnOneSector(Single ax, Single ay, Single bx, Single by)
+        {
+            var result = SpatialPartioningHelper.GetSectors(new Vector2(ax, ay), new Vector2(bx, by));
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result[0], Is.EqualTo("0:0"));
+        }
+
+        [Test]
+        public void GetSectors_TwoPointsInOneRow_ShouldReturnSectorsFromThatRow()
+        {
+            var result = SpatialPartioningHelper.GetSectors(new Vector2(168, 25), new Vector2(400, 100));
+            Assert.That(result.Count, Is.EqualTo(3));
+            Assert.That(result, Is.SubsetOf(new[] { "1:0", "2:0", "3:0" }));
+        }
+
+        [Test]
+        public void GetSectors_TwoPointsInOneColumn_ShouldReturnSectorsFromThatColumn()
+        {
+            var result = SpatialPartioningHelper.GetSectors(new Vector2(64, 64), new Vector2(64, 145));
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result, Is.SubsetOf(new[] { "0:0", "0:1" }));
+        }
+
+        [Test]
+        public void GetSectors_TwoPointsInOneLine_ShouldReturnSectorsFromThatColumn()
+        {
+            var result = SpatialPartioningHelper.GetSectors(new Vector2(64, 64), new Vector2(64, 192));
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result, Is.SubsetOf(new[] { "0:0", "0:1" }));
+        }
+
+        [Test]
+        [TestCase(64, 96, 192, 192, "0:0", "0:1", "1:1")]
+        [TestCase(416, 479, 193, 65, "1:0", "1:1", "2:1", "2:2", "2:3", "3:3")]
+        [TestCase(96, 448, 480, 224, "0:3", "1:3", "1:2", "2:2", "3:2", "3:1")]
+        public void GetSectors_TwoPointsInDifferentRowsAndColumns_ShouldReturnCorrectSectors
+            (Single ax, Single ay, Single bx, Single by, params String[] exptectedSectors)
+        {
+            var result = SpatialPartioningHelper.GetSectors(new Vector2(ax, ay), new Vector2(bx, by));
+            Assert.That(result.Count, Is.EqualTo(exptectedSectors.Length));
+            Assert.That(result, Is.SubsetOf(exptectedSectors));
+        }
+    }
+}
--- ExplainingEveryString.Core/Collisions/SpatialPartioningHelper.cs
+++ ExplainingEveryString.Core/Collisions/SpatialPartioningHelper.cs
@@ -8,10 +8,35 @@ namespace ExplainingEveryString.Core.Collisions
     internal static class SpatialPartioningHelper
     {
         private const Int32 SectorWidth = 128;
+        private const Int32 SectorHeight = 128;
 
         internal static String GetSector(Vector2 point)
         {
             return Sector(Row(point.Y), Column(point.X));
+        }
+
+        internal static List<String> GetSectors(Vector2 a, Vector2 b)
+        {
+            Vector2 left, right;
+            if (a.X < b.X)
+                (left, right) = (a, b);
+            else
+                (left, right) = (b, a);
+            var result = new List<String>();
+            for (var column = Row(left.X); column <= Row(right.X); column += 1)
+            {
+                var leftColumnBorder = System.Math.Max(left.X, column * SectorWidth);
+                var rightColumnBorder = System.Math.Min(right.X, (column + 1) * SectorWidth);
+                var leftColumnBorderHeight = left.X == right.X ? left.Y
+                    : left.Y + (leftColumnBorder - left.X) / (right.X - left.X) * (right.Y - left.Y);
+                var rightColumnBorderHeight = left.X == right.X ? right.Y
+                    : left.Y + (rightColumnBorder - left.X) / (right.X - left.X) * (right.Y - left.Y);
+                var bottom = Row(leftColumnBorderHeight < rightColumnBorderHeight ? leftColumnBorderHeight : rightColumnBorderHeight);
+                var top = Row(leftColumnBorderHeight > rightColumnBorderHeight ? leftColumnBorderHeight : rightColumnBorderHeight);
+                for (var row = bottom; row <= top; row += 1)
+                    result.Add(Sector(row, column));
+            }
+            return result;
         }
 
         internal static List<String> GetSectors(Hitbox hitbox)
@@ -27,7 +52,7 @@ namespace ExplainingEveryString.Core.Collisions
 
         private static Int32 Column(Single x) => (Int32)(x / SectorWidth);
 
-        private static Int32 Row(Single y) => (Int32)(y / SectorWidth);
+        private static Int32 Row(Single y) => (Int32)(y / SectorHeight);
 
         private static String Sector(Int32 row, Int32 column) => $"{column}:{row}";
     }
