92d72d2ff03ba2f4fb725952d00885f30b45a08a
chelovekbeznika <chelovekbeznika@yandex.ru>
2022-05-11 21:14:32 +0300

    Minimap is transparent now.

--- ExplainingEveryString.Core/Interface/Minimap/MiniMapDisplayer.cs
+++ ExplainingEveryString.Core/Interface/Minimap/MiniMapDisplayer.cs
@@ -1,8 +1,11 @@
 ﻿using ExplainingEveryString.Core.Displaying;
 using ExplainingEveryString.Core.Tiles;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 using System;
+using System.Collections.Generic;
 
 namespace ExplainingEveryString.Core.Interface.Minimap
 {
@@ -11,10 +14,14 @@ namespace ExplainingEveryString.Core.Interface.Minimap
         private const Int32 MinimapSize = 256;
         private readonly TiledMapRenderer renderer;
         private readonly Matrix minimapPositioning;
+        private readonly GraphicsDevice graphicsDevice;
+        private readonly TiledMap tiledMap;
 
         internal MiniMapDisplayer(TileWrapper map, Game gameApp)
         {
-            this.renderer = new TiledMapRenderer(gameApp.GraphicsDevice, map.TiledMap);
+            this.graphicsDevice = gameApp.GraphicsDevice;
+            this.renderer = new TiledMapRenderer(graphicsDevice, map.TiledMap);
+            this.tiledMap = map.TiledMap;
             var scale = System.Math.Max(map.Bounds.Width / MinimapSize, map.Bounds.Height / MinimapSize);
             var mapHeight = map.Bounds.Height / scale;
             this.minimapPositioning = Matrix.CreateScale(1F / scale) * 
@@ -28,7 +35,17 @@ namespace ExplainingEveryString.Core.Interface.Minimap
 
         internal void Draw()
         {
+            var savedOpacity = new Dictionary<String, Single>();
+            foreach (var layer in tiledMap.Layers)
+            {
+                savedOpacity.Add(layer.Name, layer.Opacity);
+                layer.Opacity = 0.5F;
+            }
+                
             renderer.Draw(minimapPositioning);
+
+            foreach(var layer in tiledMap.Layers)
+                layer.Opacity = savedOpacity[layer.Name];
         }
     }
 }
