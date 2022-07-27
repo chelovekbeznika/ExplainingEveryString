1aef7b3ea1787b67dd026fcdca1e019ee21cb0f9
chelovekbeznika <chelovekbeznika@yandex.ru>
2022-07-27 22:06:42 +0300

    Added map of level 5-2.

--- Assets/Ready/TileSets/5x.tsx
+++ Assets/Ready/TileSets/5x.tsx
@@ -241,6 +241,31 @@
    <property name="Pit" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="80">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="92">
   <properties>
    <property name="Pit" type="bool" value="true"/>
@@ -261,6 +286,31 @@
    <property name="Pit" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="96">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="108">
   <properties>
    <property name="Pit" type="bool" value="true"/>
@@ -269,6 +319,26 @@
  <tile id="109">
   <properties>
    <property name="Pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="pit" type="bool" value="true"/>
   </properties>
  </tile>
 </tileset>
