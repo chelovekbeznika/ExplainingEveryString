639ce29ce5542683887dccaad854f2dc1da46566
chelovekbeznika <chelovekbeznika@protonmail.com>
2023-02-17 22:18:25 +0200

    Added last level (without boss for now).

---
+++ Assets/Ready/TileSets/6x.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="6x_tiles" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="6x_tiles.png" width="256" height="256"/>
+ <tile id="2">
+  <properties>
+   <property name="Pit" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
